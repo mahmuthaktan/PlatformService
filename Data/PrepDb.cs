213f23966f503d6bb1e0c13ed3d2d0d65a9d1430
mahmuthaktan <mahmuthaktan@gmail.com>
2022-05-24 19:36:41 +0300

    .

--- Data/PrepDb.cs
+++ Data/PrepDb.cs
@@ -1,29 +1,39 @@
+using System;
+using System.Net;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using PlatformService.Models;
 
 namespace PlatformService.Data 
 {
-    public static   class PrepDb 
+    public static class PrepDb 
     {
-        private static void SeedData(AppDbContext context)
+        public static void PrepPopulation(IApplicationBuilder app)
+        {
+            using(var serviceScope = app.ApplicationServices.CreateScope())
+            {
+                SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());
+            }
+        }
+        private static void SeedData(AppDbContext context)        
         {
             if(!context.Platforms.Any())
             {
-                context.Platforms.AddRange(
-                    new Platform { Name = "Windows" },
-                    new Platform { Name = "Linux" },
-                    new Platform { Name = "MacOS" }
-                );
-                context.SaveChanges();
+                 Console.WriteLine("--> Seeding data...");
+                 context.Platforms.AddRange(
+                     new Platform { Name = "Dot Net", Publisher = "Microsoft",Cost = "Free" },
+                     new Platform { Name = "Sql Server Express", Publisher = "Microsoft",Cost = "Free" },
+                     new Platform { Name = "Kubernetes", Publisher = "Cloud Native Computing",Cost = "Free" }
+                 );
+                 context.SaveChanges();     
             }
-        }
-public static void PrepPopulation(IApplicationBuilder app)
-{
-    using (var serviceScope = app.ApplicationServices.CreateScope())
-    {
-      SeedData(serviceScope.ServiceProvider.GetService<AppDbContext>());  
+            else 
+            {
+               Console.WriteLine("--> we allready have data");
+            }
+        }     
     }
-}
-    }
-}
+} // end of namespace   PlatformService.Data    //  
+
   
